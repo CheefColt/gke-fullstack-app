11e39441e0352b23e745ca073f0a9574e827c218
cheefcolt <chiefcoltoriginal@gmail.com>
2025-04-15 17:01:21 +0000

    Change the file to listen to certain port

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend running on port ${PORT}`);
 });
